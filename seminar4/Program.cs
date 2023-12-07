81a24948ab0cf0e349f6af1494235d40a1cc7096
Василий Кузнецов <vasekku25@gmail.com>
2023-12-07 20:13:44 +0700

    Разбор 1 задачи семинара о простых числах

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -12,15 +12,58 @@
 // int m = int.Parse(Console.ReadLine()!);
 // Console.WriteLine($"Наибольшее значение: {maxNumber(n, m)}");
 
+// void inputArray(int[] arr){
+//     for (int i = 0; i < arr.Length; i++)
+//         arr[i] = new Random().Next(-100, 100);
+
+// }
+// Console.Clear();
+// Console.Write("Введите кол. элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int[] array = new int[n];
+// Console.Write($"Начальный массив: [{string.Join(", ", array)}] ");
+// inputArray(array);
+// Console.Write($"Конечный массив: [{string.Join(", ", array)}] ");
+
+
+// Напишите программу, которая на вход принимает натуральное число N, 
+// а на выходе показывает его цифры через запятую.
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// while (n > 0){
+//     int x = n % 10;
+//     Console.Write($"{x}, ");
+//     n /= 10;
+
+// }
+// 567 : 10 = 56( ост 7)
+// 567 : 10 = 56
+
+// 56 : 10 = 5(ост. 6)
+// 56 : 10 = 5
+
 void inputArray(int[] arr){
     for (int i = 0; i < arr.Length; i++)
-        arr[i] = new Random().Next(-100, 100);
+        arr[i] = new Random().Next(0, 101); //[0. 100]
 
+bool isPrime(int n){
+    for (int i = 2; i <= n / 2; i++ ){
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
 }
 Console.Clear();
 Console.Write("Введите кол. элементов массива: ");
 int n = int.Parse(Console.ReadLine()!);
 int[] array = new int[n];
-Console.Write($"Начальный массив: [{string.Join(", ", array)}] ");
 inputArray(array);
-Console.Write($"Конечный массив: [{string.Join(", ", array)}] ");
+Console.Write($"Начальный массив: [{string.Join(", ", array)}] ");
+for (int i = 0; i < array.Length; i++){
+
+    if (isPrime(array[i]) == true)
+        Console.Write($"{array[i]}");
+
+}
